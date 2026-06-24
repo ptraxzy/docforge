9fd02fc380824d2e4a3acc583386d959f36d0e1a
ultramaxo <putraagifary12@gmail.com>
2026-06-25 04:01:37 +0700

    chore: use nginx reverse proxy path /docforge

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const CONFIG_DIR = path.join(process.env.HOME || '/root', '.docforge');
 const CONFIG_FILE = path.join(CONFIG_DIR, 'config.json');
 
-const DEFAULT_SERVER_URL = 'http://ip.lubu.biz.id:8000';
+const DEFAULT_SERVER_URL = 'http://ip.lubu.biz.id/docforge';
 
 export function getConfig() {
   try {
